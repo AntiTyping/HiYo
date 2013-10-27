62a4099e0410a527649720e7de91cd346e4ce37f
Andy Pliszka <andy@isp44.com>
2013-10-27 11:48:46 -0400

    Cleans up the e2e test

--- test/e2e/main.js
+++ test/e2e/main.js
@@ -4,38 +4,37 @@
 
 describe('ToDo App', function() {
 
+  beforeEach(function() {
+    browser().navigateTo('/proxy/');
+  });
+
   describe("ToDo list", function() {
     it('should display list of items', function() {
-      browser().navigateTo('/proxy/');
-      expect(repeater('tr').count()).toBe(4);
+      expect(repeater('tr.item').count()).toBe(3);
       expect(element('tr:nth-child(1)').text()).toMatch(/HTML5 Boilerplate/);
     });
   });
 
   describe("Add item button", function() {
     it('should have placeholder text', function() {
-      browser().navigateTo('/proxy/');
       expect(element('[ng\\:model="item.name"]').attr('placeholder')).toEqual("Add new item here");
     });
   });
 
   describe("Add item button", function() {
     it('should add a new item', function() {
-      browser().navigateTo('/proxy/');
       input('item.name').enter("New item");
       element('button.js-add').click();
       expect(element('tr:last').text()).toMatch(/New item/);
     });
 
     it('should clear the new item box', function() {
-      browser().navigateTo('/proxy/');
       input('item.name').enter("New item");
       element('button.js-add').click();
       expect(input('item.name').val()).toEqual('');
     });
 
     it('should set tags to high', function() {
-      browser().navigateTo('/proxy/');
       input('item.name').enter("New item");
       element('button.js-add').click();
       expect(input('item.tags').val()).toEqual('high');
@@ -43,29 +42,25 @@ describe('ToDo App', function() {
   });
 
   it('should remove a last item', function() {
-    browser().navigateTo('/proxy/');
     element('button:last').click();
-    expect(repeater('tr').count()).toBe(3);
+    expect(repeater('tr.item').count()).toBe(2);
   });
 
 
   describe("Navigation", function() {
     describe("Home", function() {
       it("should navigate to home page", function() {
-        browser().navigateTo('/proxy/');
         element("a:contains('Home')").click();
         expect(browser().location().url()).toBe("/");
       });
 
       it("should display about page", function() {
-        browser().navigateTo('/proxy/#/');
         expect(element(".js-main").text()).toMatch(/Task/);
       });
     });
 
     describe("About", function() {
       it("should navigate to about page", function() {
-        browser().navigateTo('/proxy/');
         element("a:contains('About')").click();
         expect(browser().location().url()).toBe("/about");
       });
@@ -79,7 +74,6 @@ describe('ToDo App', function() {
 
   describe("Tags", function() {
     it("should display tags", function() {
-      browser().navigateTo('/proxy/');
       expect(element("span.tags:first").text()).toMatch(/high/);
     });
   });
@@ -87,9 +81,8 @@ describe('ToDo App', function() {
   describe("Filters", function() {
     describe("Keywords", function() {
       it("should find an item", function() {
-        browser().navigateTo('/proxy/');
         input("query.name").enter("karma");
-        expect(repeater('tr').count()).toBe(2);
+        expect(repeater('tr.item').count()).toBe(1);
       });
     });
 
@@ -97,33 +90,29 @@ describe('ToDo App', function() {
       describe("filters", function() {
         describe("high", function() {
           it("should display tagged items only", function() {
-            browser().navigateTo('/proxy/');
             element("a.tagFilter:contains('high')").click();
-            expect(repeater('tr').count()).toBe(2);
+            expect(repeater('tr.item').count()).toBe(1);
           });
         });
 
         describe("medium ", function() {
           it("should display tagged items only", function() {
-            browser().navigateTo('/proxy/');
             element("a.tagFilter:contains('medium')").click();
-            expect(repeater('tr').count()).toBe(2);
+            expect(repeater('tr.item').count()).toBe(1);
           });
         });
 
         describe("low", function() {
           it("should display tagged items only", function() {
-            browser().navigateTo('/proxy/');
             element("a.tagFilter:contains('low')").click();
-            expect(repeater('tr').count()).toBe(2);
+            expect(repeater('tr.item').count()).toBe(1);
           });
         });
 
         describe("All", function() {
           it("should display all items" , function() {
-            browser().navigateTo('/proxy/');
             element("a.tagFilter:contains('All')").click();
-            expect(repeater('tr').count()).toBe(4);
+            expect(repeater('tr.item').count()).toBe(3);
           });
         });
       });
@@ -131,15 +120,13 @@ describe('ToDo App', function() {
 
     describe("clear search button", function() {
       it("should show all the tasks", function() {
-        browser().navigateTo('/proxy/');
         input("query.name").enter("karma");
-        expect(repeater('tr').count()).toBe(2);
+        expect(repeater('tr.item').count()).toBe(1);
         element(".js-clear").click();
-        expect(repeater('tr').count()).toBe(4);
+        expect(repeater('tr.item').count()).toBe(3);
       });
 
       it("should clear the search box", function() {
-        browser().navigateTo('/proxy/');
         input("query.name").enter("karma");
         element(".js-clear").click();
         expect(input("query.name").val()).toEqual('');
